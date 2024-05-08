a512c66650cf7158c0b0f5f0dbe73f382d4d73aa
RealRavip <real862545@gmail.com>
2024-05-08 15:26:38 +0700

    Revert "revert"
    This reverts commit e0fb636ad58e315bf5fb29e21c8edabca6df5be5.

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'package:weather_station/Models/globals.dart' as global;
+import 'package:dcdg/dcdg.dart';
+
+
+// this is a test i wadjlksdk
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  global.FCMToken = fcmToken!;
+  print(global.FCMToken);
+  final Id = await _getId();
+  global.DeviceId = Id!;
+  print(global.DeviceId);
+  FirebaseMessaging.instance.onTokenRefresh
+    .listen((fcmToken) {
+      // TODO: If necessary send token to application server.
+
+      // Note: This callback is fired at each app startup and whenever a new
+      // token is generated.
+    })
+    .onError((err) {
+      // Error getting token.
+    });
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+  Future<String?> _getId() async {
+  var deviceInfo = DeviceInfoPlugin();
+  if (Platform.isIOS) {
+    var iosDeviceInfo = await deviceInfo.iosInfo;
+    return iosDeviceInfo.identifierForVendor; // unique ID on iOS
+  } else if (Platform.isAndroid) {
+    var androidDeviceInfo = await deviceInfo.androidInfo;
+    return androidDeviceInfo.id; // unique ID on Android
+  }
+}
