3271edfee8d57da6735d6a89f993e43413c52a31
RealRavip <real862545@gmail.com>
2024-05-08 15:00:07 +0700

    Revert "delete firebase file"
    This reverts commit 8def2578690abd976755044a1133fbfc45bffdf5.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_station/firebase_options.dart';
 import 'homepage.dart';
 import 'package:weather_station/Models/globals.dart' as global;
 import 'package:dcdg/dcdg.dart';
@@ -15,28 +16,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 // this is a test i wadjlksdk
 
-Future <void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  global.FCMToken = fcmToken!;
-  print(global.FCMToken);
-  final Id = await _getId();
-  global.DeviceId = Id!;
-  print(global.DeviceId);
-  FirebaseMessaging.instance.onTokenRefresh
-    .listen((fcmToken) {
-      // TODO: If necessary send token to application server.
 
-      // Note: This callback is fired at each app startup and whenever a new
-      // token is generated.
-    })
-    .onError((err) {
-      // Error getting token.
-    });
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
